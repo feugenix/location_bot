ff3fb665807f78b7754e9e38e7ff564cdb622605
Evgeny Bordunov <feugenix@yandex-team.ru>
2015-09-07 01:46:25 +0300

    Bot's code
    And file for deploy to Heroku.

--- app.js
+++ app.js
@@ -1,21 +1,122 @@
 "use strict";
-var https = reqiure('https'),
+var https = require('https'),
+    url = require('url'),
+    querystring = require('querystring'),
     express = require('express'),
+    bodyParser = require('body-parser'),
     app = express();
 
-const URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/`;
-const HOOK_URL = 'https://example.com';
-const LOCATION = process.env.LOCATION;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const API_HOST = 'api.telegram.org';
+const API_BOT_TOKEN = `/bot${BOT_TOKEN}`;
+const API_URL = `https://${API_HOST}${API_BOT_TOKEN}`;
+const APP_URL = 'https://safe-inlet-1186.herokuapp.com';
+const HOOK_URL = `/hook/${BOT_TOKEN}/`;
+const CODE_LOCATION = (process.env.CODE_LOCATION || '')
+    .split(';')
+    .map((coord) => toRadians(parseFloat(coord)));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 https
-    .get(`${URL}/setWebhook/url=${HOOK_URL}`)
+    .get(API_URL + '/setWebhook?url=' + APP_URL + HOOK_URL)
     .on('error', () => console.log('Cant set webhook'))
     .on('response', function (response) {
         if (response.statusCode !== 200)
             return console.log('Telegram sad no');
 
+        console.log('hook was set');
+
         startServer();
     });
+
+app.post(HOOK_URL, function(req, res) {
+    var message = req.body.message,
+        playerLocation = message.location,
+        textReply;
+
+    if (!playerLocation) {
+        textReply = 'Не понял';
+
+        if (message.text.length > 0) {
+            switch (message.text.toLowerCase()) {
+                case '/help':
+                    textReply = 'Абсолютный ноль (дальше 150 метров) -> Ооочень холодно (от 150 метров до 100) -> Ниже нуля (от 100 до 50 метров) -> Холодно (от 50 метров до 30) -> Тепло (от 30 метров до 15) -> Жарко (от 15 метров до 5) -> АдЪ';
+                break;
+
+                case '/fail':
+                    textReply = 'Лузер! Иди на перекрёсток Горького и Пушкина';
+                break;
+
+                case 'бегемотобар':
+                case 'бегемот':
+                    textReply = 'Правильно!';
+                break;
+            }
+        }
+
+        sendMessage({
+            chat_id: message.chat.id,
+            text: textReply
+        });
+
+        res.status(200).end();
+        return;
+    }
+
+    var resJSON = {
+        chat_id: message.chat.id,
+        text: getMessageByLocation(playerLocation)
+    };
+
+    sendMessage(resJSON);
+
+    res.status(200).end();
+});
+
+function getMessageByLocation(playerLocation) {
+    var plLatitude = toRadians(parseFloat(playerLocation.latitude)),
+        plLongitude = toRadians(parseFloat(playerLocation.longitude)),
+        playerDistance = Math.acos(
+            Math.sin(CODE_LOCATION[0]) * Math.sin(plLatitude)
+            + Math.cos(CODE_LOCATION[0]) * Math.cos(plLatitude) * Math.cos(CODE_LOCATION[1] - plLongitude)
+            ) * 6369000;
+
+    if (playerDistance <= 5) {
+        return 'АдЪ, введи название заведения';
+    } else if (playerDistance <= 15) {
+        return 'Жарко';
+    } else if (playerDistance <= 30) {
+        return 'Тепло'
+    } else if (playerDistance <= 50) {
+        return 'Холодно';
+    } else if (playerDistance <= 100) {
+        return 'Ниже нуля';
+    } else if (playerDistance <= 150) {
+        return 'Ооочень холодно';
+    } else {
+        return 'Абсолютный ноль, ищи где-то в центре';
+    }
+}
+
+function toRadians(angle) {
+    return angle * Math.PI / 180;
+}
+
+function sendMessage(message) {
+    var httpsReq = https.request({
+        hostname: API_HOST,
+        path: API_BOT_TOKEN + '/sendMessage',
+        port: 443,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+    httpsReq.end(querystring.stringify(message));
+}
 
 function startServer() {
     app.listen(process.env.PORT || 55555, () => console.log('Started'));
